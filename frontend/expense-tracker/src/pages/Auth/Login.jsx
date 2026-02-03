c2030b7c1cdb12514d6874f4dced17dfc189fd25
chadrakant18 <23d51.chadrakant@sjec.ac.in>
2026-02-03 19:03:41 +0530

    design of login page

--- frontend/expense-tracker/src/pages/Auth/Login.jsx
+++ frontend/expense-tracker/src/pages/Auth/Login.jsx
@@ -1,11 +1,56 @@
-import React from "react";
-
+import React, {  useState } from "react";
+import AuthLayout from "../../components/layouts/AuthLayout";
+import {Link, useNavigate} from 'react-router-dom';
+import Input from "../../components/Inputs/Input";
 const Login = () => {
+
+  const [email,setEmail]=useState("");
+  const [password,seetPassword]=useState("");
+  const [error,setError]=useState(null);
+
+  const navigate=useNavigate();
+
+  const handleLogin=async(e)=>{
+
+  }
   return (
-    <div>
-      <h3>Welcome Back</h3>
-      <p>Please enter your details to login</p>
-    </div>
+    <AuthLayout>
+      <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
+        <h3 className="text-xl font-semibold text-black">Welcome Back</h3>
+        <p className="text-xs text-slate-700 mt-[5px] mb-6">
+          Please enter your details to login
+        </p>
+        <form onSubmit={handleLogin}>
+          <Input 
+          value={email}
+          onChange={({target})=>setEmail(target.value)}
+          label="Email Address"
+          placeholder="john@example.com"
+          type="text"
+          />
+          <Input 
+          value={password}
+          onChange={({target})=>seetPassword(target.value)}
+          label="password"
+          placeholder="Min 8 Characters"
+          type="password"
+          />
+
+          {error&&<p className="text-red-500 text-xs pb-2.5">{error}</p>}
+          <button type="submit" className="btn-primary">
+            LOGIN
+          </button>
+
+          <p className="text-[13px] text-slate-800 mt-3">
+            Dont have an account?{""}
+            <Link className="font-medium text-primary underline" to="/signup">
+            SignUp
+            </Link>
+          </p>
+
+        </form>
+      </div>
+    </AuthLayout>
   );
 };
 
