9ea0ca1054f386fd42a6c42be6b2cc46211a016a
chadrakant18 <23d51.chadrakant@sjec.ac.in>
2026-02-03 16:15:34 +0530

    Remove node_modules and add gitignore

---
+++ frontend/expense-tracker/src/pages/Auth/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <h3>Welcome Back</h3>
+      <p>Please enter your details to login</p>
+    </div>
+  );
+};
+
+export default Login;
