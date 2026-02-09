220e362532df46751254846a4722b95c4afe1452
chadrakant18 <23d51.chadrakant@sjec.ac.in>
2026-02-09 23:28:43 +0530

    mongodb connection

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express=require("express");
 const cors=require("cors");
 const path=require("path");
-
+const connectDB=require("./config/db");
 const app=express();
 
 app.use(
@@ -15,7 +15,8 @@ app.use(
 );
 
 app.use(express.json());
-
+3
+connectDB();
 const PORT=process.env.PORT||5000;
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`);
