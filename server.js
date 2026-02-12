f82838d879b72e25eb559014194eaf3239168e28
Chandrakant <23d51.chadrakant@sjec.ac.in>
2026-02-12 11:07:45 +0530

    Removed frontend files

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express=require("express");
 const cors=require("cors");
 const path=require("path");
 const connectDB=require("./config/db");
+const authRoutes=require("./routes/authRoutes");
 const app=express();
 
 app.use(
@@ -15,8 +16,10 @@ app.use(
 );
 
 app.use(express.json());
-3
+
 connectDB();
+
+app.use("/api/v1/auth",authRoutes);
 const PORT=process.env.PORT||5000;
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`);
