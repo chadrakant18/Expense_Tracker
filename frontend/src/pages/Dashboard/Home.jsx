372c715b80fc4ebb1509d941cca413ec822011ca
Chandrakant <23d51.chadrakant@sjec.ac.in>
2026-02-21 14:49:04 +0530

    dashboard features

--- frontend/src/pages/Dashboard/Home.jsx
+++ frontend/src/pages/Dashboard/Home.jsx
@@ -4,7 +4,10 @@ import DashboardLayout from "../../components/layouts/DashboardLayout";
 import { useUserAuth } from "../../hooks/useUserAuth";
 import axiosInstance from "../../utils/axiosInstance";
 import { API_PATHS } from "../../utils/apiPaths";
-
+import InfoCard from "../../components/Cards/InfoCard";
+import { LuHandCoins,LuWalletMinimal } from "react-icons/lu";
+import {IoMdCard} from "react-icons/io";
+import { addThousandsSeparator } from "../../utils/helper";
 const Home = () => {
   useUserAuth();
 
@@ -40,7 +43,27 @@ const Home = () => {
   return (
     <DashboardLayout activeMenu="Dashboard">
       <div className="my-5 mx-auto">
-        
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          <InfoCard
+          icon={<IoMdCard/>}
+            label="Total Balance"
+            value={addThousandsSeparator(dashboardData?.totalBalance||0)}
+            color="bg-primary"
+            />
+             <InfoCard
+          icon={<LuWalletMinimal/>}
+            label="Total Income"
+            value={addThousandsSeparator(dashboardData?.totalIncome||0)}
+            color="bg-orange-500"
+            />
+             <InfoCard
+          icon={<LuHandCoins/>}
+            label="Total Expense"
+            value={addThousandsSeparator(dashboardData?.totalExpense||0)}
+            color="bg-red-500"
+            />
+          
+        </div>
       </div>
     </DashboardLayout>
   );
