02006c5dc0fc39bfccd97d0fc4f814608fb514ab
Chandrakant <23d51.chadrakant@sjec.ac.in>
2026-04-06 19:17:17 +0530

    API call to delete income

--- frontend/src/pages/Dashboard/Expense.jsx
+++ frontend/src/pages/Dashboard/Expense.jsx
@@ -1,13 +1,13 @@
 import React from "react";
+import { useUserAuth } from "../../hooks/useUserAuth";
 
 const Expense = () => {
+  useUserAuth();
   return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold">Expense Page</h1>
-      <p className="mt-2 text-gray-600">
-        Manage your expenses here.
-      </p>
+     <DashboardLayout activeMenu="Income">
+      <div className="my-5 max-auto">
     </div>
+    </DashboardLayout>
   );
 };
 
